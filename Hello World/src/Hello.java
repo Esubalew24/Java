475c926f78dcf27d00858998399210b851e90bda
Esubalew Workineh <esub24@yahoo.com>
2017-04-14 15:46:12 +0300

    Varaiables

--- Hello World/src/Hello.java
+++ Hello World/src/Hello.java
@@ -3,6 +3,31 @@
  */
 public class Hello {
     public static void main(String[] args){
-        System.out.print("Hello, Esubalew!");
+
+        System.out.println("Hello, Esubalew!");
+
+
+        //Variables
+
+        int myNumber = 24;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myNumber + mySecondNumber + myThirdNumber;
+
+        int randomNumber = (34 *5) + (566 - 455) + myNumber / mySecondNumber;
+
+        System.out.println(myNumber);
+        System.out.println(myNumber * myNumber);
+        System.out.println("This "+ "myNumber" + " is a string");
+        System.out.println(myNumber + " is my number ");
+
+        System.out.println(myNumber + mySecondNumber + myThirdNumber);
+        System.out.println(myTotal);
+
+        System.out.println(randomNumber);
+
+
+
+
     }
 }
