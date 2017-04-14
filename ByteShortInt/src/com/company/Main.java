c25160535fd024d2e27287d238d40ca015f42e2b
Esubalew Workineh <esub24@yahoo.com>
2017-04-14 16:27:25 +0300

    Byte + Short + Int

---
+++ ByteShortInt/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 1_000;
+        int number1 = -1234567890;
+
+        //Minimum and Maximum numbers in int data type
+        // int has a width of 32
+
+        int myMinValue = -2147483648;
+        int myMaxValue = 2147483647;
+        System.out.println(myValue);
+
+        // Byte Data type
+        // Byte has a width of 8
+
+        byte minByteNumber = -128;
+        byte maxByteNumber = 127;
+
+        //Short Data type
+        //Short has a width of 16
+        short minShortNumber = -32768;
+        short maxShortNumber = 32767;
+
+        //Long min and max value
+        // Long has a width of 64
+        long minLongNumber = -9_223_372_036_854_775_808L;
+        long maxLongNumber = 9_223_372_036_854_775_807L;
+
+
+    }
+}
