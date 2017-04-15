5ab2c589c7778779dc69ae854a01fcfe57bda8e3
Esubalew Workineh <esub24@yahoo.com>
2017-04-15 16:48:25 +0300

    Char and Boolean

---
+++ CharAndBoolean/src/CharAndBoolean.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Esubalew on 15/04/17.
+ */
+public class CharAndBoolean {
+
+    public static void main(String[] args){
+        char myChar = '\u00A9';
+        char mySecondChar = '\u00BF';
+
+        System.out.println("Unicode output was: " + myChar);
+        System.out.println("Second Unicode output was: " + mySecondChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+        boolean javaIsGreat = true;
+
+
+        // Challenge
+
+        char registeredChar = '\u00AE';
+        System.out.println("Registered Symbol = " + registeredChar);
+
+    }
+}
+
