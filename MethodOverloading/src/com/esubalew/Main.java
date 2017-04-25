e79a937302727af1227aeb5290ba9f9cc6907fb2
Esubalew Workineh <esubwork@gmail.com>
2017-04-25 11:48:57 +0300

    Method Overloading

--- MethodOverloading/src/com/esubalew/Main.java
+++ MethodOverloading/src/com/esubalew/Main.java
@@ -9,6 +9,10 @@ public class Main {
         calculateScore("Esub", 500);
         calculateScore(200);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6,0);
+        calcFeetAndInchesToCentimeters(7,5);
+        calcFeetAndInchesToCentimeters(-1,3);
+        calcFeetAndInchesToCentimeters(100);
 
         }
 
@@ -33,19 +37,27 @@ public class Main {
     // Challenge
 
     public static double calcFeetAndInchesToCentimeters(double feet , double inches){
-       if((feet < 0) || (inches < 0 && inches >12)) {
+       if((feet < 0) || (inches < 0 || inches > 12)) {
+           System.out.println("Invalid feet or inch parameter");
            return -1;
        }
 
        double centimeter = (feet * 12) * 2.54;
        centimeter +=inches *2.54;
         System.out.println(feet + " feet, " + inches + " inches= " + centimeter + " cm");
-        return 2;
+        return centimeter;
     }
 
-    public static void calcFeetAndInchesToCentimeters(int inches){
-
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if(inches < 0){
+            return -1;
+        }
+        double feet = (int) inches/12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + "inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
 
     }
 
-}
+
